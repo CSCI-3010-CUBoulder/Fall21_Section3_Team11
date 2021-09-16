e7f80a5472529a2fda524c4880b6745dcfa9b6ab
Michela Puni Nimako <michelapuni@gmail.com>
2021-09-16 16:47:57 -0600

    wrote 2 functions

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -1,1 +1,16 @@
+// Multiplies all numbers in a vector together and returns the resulting value
+double Product(std::vector<double> nums){
+    product = 0;
+    for (int i = 0; i < nums.length(); i++){
+        product *= nums[i];
+    }
+    return product;
+}
 
+// returns -1 if the number is negative and 1 if positive
+double Sign(double num){
+    if num < 0:
+        return -1
+    else:
+        return 1
+}
